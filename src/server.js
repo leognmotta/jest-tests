350ec4ee8786cd2c94aa020b7e2bdf3e855fa343
Leonardo Motta <leo@domboscoatacado.com.br>
2019-02-22 19:05:48 -0300

    Change env

--- src/server.js
+++ src/server.js
@@ -3,17 +3,17 @@ const http = require('http');
 
 const mongoose = require('mongoose');
 
-const server = http.createServer(app);
-
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-sud5s.mongodb.net/${
       process.env.DB_NAME
     }`,
-    { useNewUrlParser: true }
+    { useNewUrlParser: true, useCreateIndex: true }
   )
   .then(() => console.log('Connected to database'))
   .catch(error => console.log(error));
+
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3001, () => {
   console.log(`server listening on port ${process.env.PORT || 3001}`);
